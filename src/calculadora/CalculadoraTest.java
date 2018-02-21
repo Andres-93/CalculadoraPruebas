ebcae35e27b4ee75df1788ceff49fc058a1aa22f
Andres-93 <andres.pgomez93@gmail.com>
2018-02-21 11:32:12 +0100

    Nueva Version

--- src/calculadora/CalculadoraTest.java
+++ src/calculadora/CalculadoraTest.java
@@ -2,13 +2,32 @@ package calculadora;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculadoraTest {
 
+	
+	Calculadora pete;
+	
+	@Before
+	public void creaCalculadora() {
+		pete = new Calculadora(20,30);
+		System.out.println("Inicio prueba");
+		
+	}
+	@After
+	public void creaCalculadora2() {
+		
+		System.out.println("Fin de la prueba");
+		
+	}
+	
+	
 	@Test
 	public void testSuma() {
-		Calculadora pete = new Calculadora(20,30);
+
 		int num = pete.suma();	
 		assertEquals(50,num);
 		
@@ -17,39 +36,62 @@ public class CalculadoraTest {
 
 	@Test
 	public void testResta() {
-		Calculadora pete = new Calculadora(20,30);
+
 		int num = pete.resta();	
-		assertEquals(-10,num);
+		assertEquals(10,num);		
+	}
+	
+	@Test
+	public void testNum1EsMayor() {
+
+		boolean resultado = pete.num1EsMayor();
+		
+		//assertEquals(false,resultado)
+		//assertTrue("no es verdadero,resultado)
+		//En este caso el valor esperado es false, si buscaramos un ture usariamos el assertTrue
+		assertFalse(resultado);
+		
 		
 	}
 
 	@Test
 	public void testMultiplicacion() {
-		Calculadora pete = new Calculadora(20,30);
 		int num = pete.multiplicacion();	
 		assertEquals("fallo en la multiplicacion",600,num);
 	}
 
 	@Test
 	public void testDivision() {
-		Calculadora pete = new Calculadora(8,2);
 		int num = pete.division();	
-		assertEquals("fallo en la division",4,num);
+		assertEquals("fallo en la division",0,num);
+	}
+	
+	@Test
+	public void testDivision2() {
+		Integer resultado = pete.division2();
+		assertNotNull(resultado);
+		
 	}
 	
 	@Test
 	public void testDivisionPorCero() {
-		Calculadora pete = new Calculadora(8,0);
 		int num = 0;
 		try {
 			 num = pete.division();	
-			fail(("FALLO, deberia haber lanzado la excepcion"));
 		}catch(ArithmeticException error) {
 			System.out.println(error.getMessage());
-		}
-
-	
+			fail(("FALLO, deberia haber lanzado la excepcion"));
+		}	
 	}
+	/*
+	@Test(expected = java.lang.ArithmeticException.class)
+	public void testDivision3() {	
+		int num = pete.division3();
+		
+		
+	}
+	*/
+	
 }
 	
 
